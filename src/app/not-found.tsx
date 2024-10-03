8f0e7f3f8836b92beca5bca4989f9fab1042ab2b
ciceroserafim <ciceroserafim777@mail.com>
2024-10-02 22:21:09 -0300

    Not-Found finalizado

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Image from "next/image";
+import "@/styles/globals.css";
 
-export default function NotFound() {
+export default function Custom404() {
   return (
-    <div>
+    <div className="notfound-container">
+      <h1 className="notfound-title">404 - Página Não Encontrada</h1>
+      <h2 className="notfound-subtitle">Desculpe, esta página não existe.</h2>
+      <Image
+        src="https://lojaintegrada.com.br/hub/wp-content/uploads/2023/05/erro-404-4.webp"
+        alt="Página de erro"
+        width={400}
+        height={400}
+        className="notfound-image"
+      />
     </div>
-  )
+  );
 }
