69d282652591d0b6eca95f36a4f4ce33479adeb8
IsabellaMedeiros1 <isabelladefatimamedeiros@gmail.com>
2024-10-02 22:53:55 -0300

    ajustando informações do footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,27 @@
-import "@/styles/globals.css"
+import "@/styles/globals.css";
 
-export default  function Footer(){
-  
+export default function Footer() {
   return (
     <footer className="rodape">
-      <p>&copy; 2024 NASA Theories Explorer. Todos os direitos reservados.</p>
+      <div className="container mx-auto py-4">
+        <p className="text-center">
+          Projeto desenvolvido por:
+          <br />
+          Isabella de Fátima Medeiros (RM: 554725)
+          <br />
+          Eduardo Miguel Forato Monteiro (RM: 554725)
+          <br />
+          Cicero Gabriel Oliveira Serafim (RM: 556745)
+        </p>
+        <p className="text-center mt-4">
+          Todos os direitos reservados.
+        </p>
+        <p className="text-center mt-2">
+          <a href="https://github.com/IsabellaMedeiros1/front-CP2" className="text-gray-400 hover:text-gray-200">
+            Repositório GitHub
+          </a>
+        </p>
+      </div>
     </footer>
   );
-};
-
+}
