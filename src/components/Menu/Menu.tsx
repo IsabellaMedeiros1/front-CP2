2245d10e11a6b90aa65fcce5d006a39b0eeddb55
ciceroserafim <ciceroserafim777@mail.com>
2024-10-02 22:25:05 -0300

    Menu Criado

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import Link from 'next/link';
+import "@/styles/globals.css"
 
-export default function Menu() {
+export default function Menu(){
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav>
+      <ul className="menu">
+        <li><Link href="/" className='active'>Home</Link></li>
+        <li><Link href="/paginas/orbita">Órbitas Planetárias</Link></li>
+        <li><Link href="/paginas/cronologia">Cronologia dos Eventos</Link></li>
+        <li><Link href="/paginas/forca">Forças Gravitacionais</Link></li>
+        <li><Link href="/paginas/velocidade">Velocidade e Trajetória</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
