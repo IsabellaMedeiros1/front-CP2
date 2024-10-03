1cefe49dad7a9fc919da98c07b321a2ad0c583bc
edumiguel <dudumfmonteiro@gmail.com>
2024-10-02 22:29:53 -0300

    Page finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import "@/styles/globals.css"
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+      <main className="home-container">
+          <h1 className="home-title">Teorias de Immanuel Velikovsky</h1>
+          <p className="home-text">
+              No livro <strong>Worlds in Collision</strong> (1950), Immanuel Velikovsky propôs diversas teorias controversas envolvendo eventos cósmicos que, segundo ele, teriam afetado a Terra no passado. Ele sugeriu que fenômenos astronômicos descritos em textos antigos poderiam ser explicados por eventos cataclísmicos, como colisões ou passagens próximas de corpos celestes, incluindo <strong>Vênus</strong> e <strong>Marte</strong>.
+          </p>
+          <p className="home-text">
+              Embora Velikovsky tenha utilizado referências históricas e mitológicas para sustentar suas ideias, ele também levantou questões sobre os cálculos e modelos astronômicos tradicionais. Alguns dos principais aspectos que Velikovsky sugeriu ou questionou em seu trabalho incluem:
+          </p>
+          <ul className="home-list">
+              <li>órbitas Planetárias</li>
+              <li>Cronologia dos Eventos</li>
+              <li>Forças Gravitacionais</li>
+              <li>Velocidade e Trajetória</li>
+          </ul>
+      </main>
+  );
 }
+
+
+
+
+
+
